0a170c0a2936957f45505e68271e70a6f5d0e779
Safiya Cain <say.cain3@gmail.com>
2020-12-28 17:25:12 -0500

    set up product page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import SignupFormPage from "./components/SignupFormPage";
 // import LoginFormPage from "./components/LoginFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import Productpage from "./components/ProductPage/Product";
 
 function App() {
   const dispatch = useDispatch();
@@ -23,6 +24,9 @@ function App() {
           </Route> */}
           <Route path="/signup">
             <SignupFormPage />
+          </Route>
+          <Route exact path="/">
+            <Productpage />
           </Route>
         </Switch>
       )}
---
+++ frontend/src/components/ProductPage/Product.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+// import * as productActions from "../../store/product";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { fetchProducts } from '../../store/product';
+import './Product.css';
+
+function Productpage(){
+    const products = useSelector(state => state.product);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+
+
+    useEffect(() => {
+            dispatch(fetchProducts());
+    }, [])
+
+
+
+    return(
+        <div className="productContainer">
+            {products.map((productItem)=>(
+                <div key={productItem.title}>
+                    {/* <h3>{productItem.title}</h3> */}
+                    <div className="image-block">
+                        <img className="images" src={productItem.image} alt={productItem.title} onClick={()=> history.push(`/product/${productItem.id}`)} />
+                        <h4>${productItem.price}</h4>
+                    </div>
+                </div>
+             ))}
+        </div>
+    )
+}
+
+export default Productpage;
