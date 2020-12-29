895f4ee7c06cd227a1a93c723a901135ca45316e
Safiya Cain <say.cain3@gmail.com>
2020-12-29 11:01:00 -0500

    create cart logic

--- frontend/src/components/Cart/ShoppingCart.js
+++ frontend/src/components/Cart/ShoppingCart.js
@@ -5,6 +5,20 @@ function ShoppingCart(){
     const product = useSelector(state => state.product);
     const dispatch = useDispatch();
 
+    const cartItems = Object.values(cart)
+        .map(item => {
+            return {
+                ...item,
+                ...product[item.id]
+            };
+        });
+    if(!cartItems || !cartItems.length) return (
+        <div className='cart'>
+            <h3>Your cart is empty.</h3>
+            <h2>Discover something unique to fill up.</h2>
+        </div>
+    )
+
 }
 
 export default ShoppingCart;
