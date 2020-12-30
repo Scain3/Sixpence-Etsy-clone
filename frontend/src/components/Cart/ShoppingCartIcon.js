cade0cbdf664f09dbda71ed84b603ba6d6e9ce46
Safiya Cain <say.cain3@gmail.com>
2020-12-30 14:18:21 -0500

    cart icon navigates to new page on click

--- frontend/src/components/Cart/ShoppingCartIcon.js
+++ frontend/src/components/Cart/ShoppingCartIcon.js
@@ -1,8 +1,9 @@
 import { useHistory } from "react-router-dom";
 
 function ShoppingCartIcon(){
+    const history = useHistory();
     return(
-        <i className="fas fa-shopping-cart" onClick={()=> history.pushState(`/cart/${productItem.id}`)}></i>
+        <i className="fas fa-shopping-cart" onClick={()=> history.push(`/cart`)}></i>
     )
 }
 
