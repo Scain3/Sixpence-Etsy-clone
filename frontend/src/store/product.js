0dbbbfa1d5b1b1893651e3b5488a9d087d34fc08
Safiya Cain <say.cain3@gmail.com>
2020-12-30 18:29:43 -0500

    create thunk action for create a product

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -4,6 +4,8 @@ const GET_PRODUCTS = 'GET_PRODUCTS';
 
 const GET_ONE_PRODUCT = 'GET_ONE_PRODUCT';
 
+const SET_PRODUCT = 'SET_PRODUCT';
+
 //GET ALL THE PRODUCTS
 const getProducts = (products) => {
     return{
@@ -12,9 +14,18 @@ const getProducts = (products) => {
     }
 }
 
+//GET ONE PRODUCT
 const getOneProduct = (product) => {
     return{
         type: GET_ONE_PRODUCT,
+        payload: product,
+    }
+}
+
+//CREATE A PRODUCT
+const setProduct = (product) => {
+    return{
+        type: SET_PRODUCT,
         payload: product,
     }
 }
@@ -25,9 +36,29 @@ export const fetchProducts = () => async(dispatch) => {
     dispatch(getProducts(response.data.product));
 }
 
+//THUNK ACTION FOR GETTING A SINGLE PRODUCT
 export const fetchSingleProduct = (id) => async(dispatch) => {
     const response = await fetch(`/api/products/${id}`);
     dispatch(getOneProduct(response.data.singleItem));
+}
+
+export const fetchSetProducts = (product) => async(dispatch) => {
+    const { userId, title, description, image, price } = product;
+
+    const response = await fetch("/api/products/add-a-listing", {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify( {
+            userId,
+            title,
+            description,
+            image,
+            price
+        })
+    });
+
+    dispatch(setProduct(response.data.productItem));
+    return response;
 }
 
 const initialState = {}
@@ -42,6 +73,8 @@ const productReducer = (state=initialState, action) => {
             return newProducts;
         case GET_ONE_PRODUCT:
             return {...state, [action.payload.id]: action.payload};
+        case SET_PRODUCT:
+            return [action.payload];
         default:
             return state;
     }
