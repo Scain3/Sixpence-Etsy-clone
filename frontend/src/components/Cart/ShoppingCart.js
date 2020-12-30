e55285c2539a26ff10b3eeb12000cd15914184b3
Safiya Cain <say.cain3@gmail.com>
2020-12-30 15:47:14 -0500

    create landing page for cart

--- frontend/src/components/Cart/ShoppingCart.js
+++ frontend/src/components/Cart/ShoppingCart.js
@@ -2,9 +2,19 @@ import {useSelector, useDispatch} from "react-redux";
 import {purchaseItems} from "../../store/cart";
 
 function ShoppingCart(){
+const cart = useSelector(state => state.cart);
 
   return(
-    <h1>This is where the shopping cart will be.</h1>
+    <div>
+    {cart.map((cartItem) => (
+      <div>
+        <h3>{cartItem.title}</h3>
+        <img src={cartItem.image} alt={cartItem.title} />
+        <p>{cartItem.price}</p>
+      </div>
+
+    ))}
+    </div>
   )
 
 }
