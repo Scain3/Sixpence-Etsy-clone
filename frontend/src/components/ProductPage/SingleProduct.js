22c4f69d477a4117fd63f6500bf166cd21296422
Safiya Cain <say.cain3@gmail.com>
2021-01-02 15:46:24 -0500

    add to cart button on single page is functioning

--- frontend/src/components/ProductPage/SingleProduct.js
+++ frontend/src/components/ProductPage/SingleProduct.js
@@ -2,23 +2,34 @@ import { useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 import { fetchSingleProduct } from "../../store/product";
+
+import { addItemToCart } from "../../store/cart";
 
 import "./Product.css";
 
 function SingleProductPage(){
+    const user = useSelector(state => state.session.user);
     const products = useSelector(state => state.products);
     const { id } = useParams();
     const dispatch = useDispatch();
-    const singleItem = products[id]
+    const singleItem = products[id];
+    const history = useHistory();
 
     useEffect(()=> {
         if(singleItem) return
         dispatch(fetchSingleProduct(id));
     }, [id])
-    
+
+    const handleClick = () => {
+        if(!user){
+         return history.push('/login');
+        }
+        dispatch(addItemToCart(singleItem.id, user.id));
+    }
+
     if(!singleItem) return null
     return(
         <div className="productContainer">
@@ -27,6 +38,7 @@ function SingleProductPage(){
                         <h1>{singleItem.title}</h1>
                         <img className="single-image" src={singleItem.image} alt={singleItem.title} />
                         <h4>${singleItem.price}</h4>
+                        <button onClick={handleClick}>Add To Cart</button>
                     </div>
                     <div className="description">
                             <p>{singleItem.description}</p>
