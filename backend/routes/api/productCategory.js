20c071e7ace19f068b2c9aad0281605f4af8d9d9
Safiya Cain <say.cain3@gmail.com>
2021-03-01 14:26:12 -0500

    create backend route to get all product categories

--- backend/routes/api/productCategory.js
+++ backend/routes/api/productCategory.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
-const { productCategory, category } = require("../../db/models");
+const { ProductCategory, category } = require("../../db/models");
+
+router.get('/', asyncHandler(async(req, res)=> {
+    const category = await ProductCategory.findAll();
+    res.json({
+        category,
+    })
+}));
