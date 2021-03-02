abcfb1ecc0ffdf2f8acdef2a49ef14b1db8ad5c2
Safiya Cain <say.cain3@gmail.com>
2021-03-02 16:26:53 -0500

    add ability to link to single page from search

--- frontend/src/components/Search/SearchProductPage.js
+++ frontend/src/components/Search/SearchProductPage.js
@@ -1,17 +1,19 @@
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import "./Search.css";
 
 
 function SearchProductPage(){
+    const history = useHistory();
     const products = useSelector(state => state.search);
 
     return(
         <div>
             {products.map((product)=>(
-                <div key={product.title}>
-                    <h1>{product.title}</h1>
+                <div key={product.title} onClick={()=>history.push(`product/${product.id}`)}>
                     <img className="images" src={product.image} alt={product.title} />
-                    <p className="description">{product.description}</p>
+                    <h1>{product.title}</h1>
+                    <h3>{product.price}</h3>
                 </div>
             ))}
         </div>
