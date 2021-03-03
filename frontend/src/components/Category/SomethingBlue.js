a01a8dd1f99cb7313f83d6c4ca3fbd3a0e846afb
Safiya Cain <say.cain3@gmail.com>
2021-03-03 12:07:53 -0500

    provide descriptions for the category pages

--- frontend/src/components/Category/SomethingBlue.js
+++ frontend/src/components/Category/SomethingBlue.js
@@ -11,6 +11,7 @@ function SomethingBlue(){
 
     return(
        <div className="category-div">
+           <p><strong>Something Blue</strong> is the last line in the wedding traditional rhyme. In this line it promotes that a bride must have something blue to bring good luck to the wedding day. Find your something blue today.</p>
            <div className="category-map">{
                blue && blue.map((blueItem, index) => (
                    <div className="blueItem-obj" key={index}>
