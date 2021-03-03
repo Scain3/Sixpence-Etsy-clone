4b029c3d30004e3506a0d313068074948c20ea1e
Safiya Cain <say.cain3@gmail.com>
2021-03-03 12:54:17 -0500

    add welcome sign to authenticated view

--- frontend/src/components/ProductPage/Product.js
+++ frontend/src/components/ProductPage/Product.js
@@ -8,8 +8,9 @@ import './Product.css';
 
 function Productpage(){
     const products = useSelector(state => state.products);
-    //const user = useSelector(state => state.user);
+    const user = useSelector(state => state.session.user);
     const dispatch = useDispatch();
+    console.log(user);
 
     useEffect(() => {
             dispatch(fetchProducts());
@@ -18,13 +19,27 @@ function Productpage(){
     if(!products){
         return null;
     }
+
     const productsArr = Object.values(products);
+    if(user){
+        return(
+            <div>
+                <h1>Welcome to Sixpence, {user.username}</h1>
+                <div className="productContainer">
+                {productsArr.map((productItem)=>(
+                    <ProductItem key={productItem.id} product={productItem} />
+                ))}
+            </div>
+            </div>
+        )
+    }
     return(
-        <div className="productContainer">
-            {productsArr.map((productItem)=>(
-                <ProductItem key={productItem.id} product={productItem} />
-             ))}
-        </div>
+            <div className="productContainer">
+                {productsArr.map((productItem)=>(
+                    <ProductItem key={productItem.id} product={productItem} />
+                ))}
+            </div>
+
     )
 }
 
