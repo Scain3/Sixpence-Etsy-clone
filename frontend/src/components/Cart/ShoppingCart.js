54d959e45f8ac6fe1607c670d7db7d99464f63b7
Safiya Cain <say.cain3@gmail.com>
2021-03-03 18:38:43 -0500

    style cart

--- frontend/src/components/Cart/ShoppingCart.js
+++ frontend/src/components/Cart/ShoppingCart.js
@@ -4,20 +4,20 @@ import './Cart.css';
 
 function CartItemsHeader({howManyItems}) {
   if (howManyItems === 1) {
-      return <h2>There is 1 item in your cart.</h2>
+      return <h2 className="cart-amount-display">There is 1 item in your cart.</h2>
   }
-  return <h2>There are {howManyItems} items in your cart.</h2>;
+  return <h2 className="cart-amount-display">There are {howManyItems} items in your cart.</h2>;
 }
 
 
 function ShoppingCart(){
   const cart = useSelector(state => state.cart);
-  
+
   console.log(cart);
   const cartArr = Object.values(cart);
   const dispatch = useDispatch();
 
-  if(cartArr.length === 0) return <h1>Your cart is empty!</h1>
+  if(cartArr.length === 0) return <h1 className="cart-amount-display">Your cart is empty!</h1>
 
 
   return(
@@ -31,7 +31,7 @@ function ShoppingCart(){
           <h3>{cartItem.title}</h3>
           <img className="images" src={cartItem.image} alt={cartItem.title} />
           <p className="price">{cartItem.price}</p>
-          <button onClick={()=> {dispatch(removeItemFromCart(cartItem.id))}}>Remove</button>
+          <button className="remove-button" onClick={()=> {dispatch(removeItemFromCart(cartItem.id))}}>Remove</button>
         </div>
 
       ))}
