07a4876a59d920ad4ab3e1c234ef048a38d2605d
Safiya Cain <say.cain3@gmail.com>
2021-03-05 13:08:18 -0500

    create demo login capability

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
 import SearchBar from '../Search/SearchBar';
 import ShoppingCartIcon from '../Cart/ShoppingCartIcon';
 import SecondNav from '../SecondNav/SecondNav.js';
+import {login} from '../../store/session';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+  const [errors, setErrors] = useState([]);
+
+  const demoLogin = () => {
+    const email = 'demo@user.io';
+    const password = 'password';
+    return dispatch(login({email, password}))
+        .catch((res) => {
+            if (res.data && res.data.errors) setErrors(res.data.errors);
+        });
+  }
 
   let sessionLinks;
   if (sessionUser) {
@@ -22,6 +34,9 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <div className="auth-links">
+        <span className=".demo-container">
+          <button className="Demo-login" onClick={demoLogin}>Demo Login</button>
+        </span>
         <LoginFormModal />
         <NavLink to="/register" className="register">Register</NavLink>
       </div>
