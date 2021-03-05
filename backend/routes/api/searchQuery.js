0488f739f9de332d285e6f3797d5cdeba37811a1
Safiya Cain <say.cain3@gmail.com>
2021-03-05 12:39:07 -0500

    update search query to listen for two more categories

--- backend/routes/api/searchQuery.js
+++ backend/routes/api/searchQuery.js
@@ -3,7 +3,11 @@ const { ProductListing }= require('../../db/models');
 
 
 const searchProducts = async (term) => {
-    return await ProductListing.findAll({where: { title: { [Op.iLike]: term }}});
+    return await ProductListing.findAll({
+        where: {
+            [Op.or]: [{title: { [Op.iLike]: term }}, {category: { [Op.iLike]: term }}, {description: { [Op.iLike]: term }}]
+        }
+    });
 }
 
 
